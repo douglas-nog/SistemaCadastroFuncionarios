a46a22692fa0f7dbd2c75ff70144b810f0168646
Douglas <douglaswnogueira@hotmail.com>
2022-08-23 18:31:23 -0300

    Criação das classes Funcionario e Gerente

---
+++ BankHerd/src/Funcionario.java
@@ -0,0 +1,41 @@
+
+public class Funcionario {
+	
+	private String nome;
+	private String cpf;
+	private String matricula;
+	protected double salario;
+	
+	//Todo funcionario receberá uma bonificação de 10% do salário.
+	
+	public double getBonificacao() {
+		return this.salario * 0.1;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+}
